37182ef8e4efee40695229d740bd63b62743551e
Marc DeSantis <marc.desantis@wwt.com>
2016-10-22 16:07:45 -0500

    Added programs

---
+++ ddl/programv.sql
@@ -0,0 +1,8 @@
+create or replace view programv as
+select program.*,
+	sum(case when enrollment.status = 'Lead' then 1 else 0 end) lead_count,
+    sum(case when enrollment.status = 'Active' then 1 else 0 end) active_count,
+    sum(case when enrollment.status = 'Success' then 1 else 0 end) succes_count
+from program
+left outer join enrollment on (program.id = enrollment.program_id)
+group by program.id;
---
+++ routes/programs.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var router = express.Router({mergeParams: true});
+const path = require('path');
+const moment = require('moment');
+var HttpStatus = require('http-status-codes');
+var _ = require('lodash');
+var programClients = require('./program_clients')
+
+router.use('/:program_id/clients', programClients);
+
+function addHref(program) {
+  if (program) {
+    program['clients_href'] = '/agencies/' + program.agency_id + '/programs/'+ program.id + '/clients';
+  }
+  return program;
+}
+
+function mod(programs) {
+  if (Array.isArray(programs)) {
+    _.forEach(programs, function(program) {
+      addHref(program);
+    });
+  } else {
+    addHref(programs);
+  }
+  return programs || {};
+}
+
+router.get('/', (req, res, next) => {
+  var db = req.app.get('db');
+  var queryParams = req.query;
+  queryParams['agency_id'] = req.params.agency_id;
+  db.programv.find(queryParams, function(err, programs){
+    return res.json(mod(programs));
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = Number(req.params.id);
+  var db = req.app.get('db');
+  db.programv.find(id, function(err, program){
+    if (program) {
+      return res.json(program);
+    } else {
+      return res.status(HttpStatus.NOT_FOUND).json({});
+    }
+  });
+});
+
+router.post('/', (req, res, next) => {
+  var db = req.app.get('db');
+  var program = req.body;
+  program['agency_id'] = req.params.agency_id;
+  program.creation_date = new Date();
+  program.last_update_date = new Date();
+  db.program.insert(program, function(err, program){
+    if (err) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
+    }
+    return res.status(HttpStatus.CREATED).json(mod(program));
+  });
+});
+
+router.put('/:id', (req, res, next) => {
+  var db = req.app.get('db');
+  var program = req.body;
+  program.last_update_date = new Date();
+  program.id = Number(req.params.id);
+  db.program.save(program, function(err, program){
+    if (err) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(err);
+    }
+    return res.status(HttpStatus.OK).json(mod(program));
+  });
+});
+
+router.delete('/:id', (req, res, next) => {
+  const results = [];
+  var db = req.app.get('db');
+  db.program.destroy({id: Number(req.params.id)}, function(err, program){
+      return res.status(HttpStatus.NO_CONTENT).json(program);
+  });
+});
+
+module.exports = router;
