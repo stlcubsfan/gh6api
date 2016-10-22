9694a848f11fcc16d8868b79e6fdf8bf6d610309
harrisch <charlie.harris@wwt.com>
2016-10-21 23:32:06 -0500

    Adds missing middleware for clients

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/agencies', agencies);
+app.use('/clients', clients);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
