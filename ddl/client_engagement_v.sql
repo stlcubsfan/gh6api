296a1f6810b9d897082867e0b43291d470a2f217
Marc DeSantis <marc.desantis@wwt.com>
2016-10-22 18:53:18 -0500

    Updated the view

--- ddl/client_engagement_v.sql
+++ ddl/client_engagement_v.sql
@@ -1,10 +1,10 @@
 create view client_engagement_v as
-select client.id client_id, opportunity_xref.id opportunity_id, opportunity_xref.partner_id,
-	   null enrollment_id, null program_id, null agency_id
+select opportunity_xref.agency_id, client.id client_id, opportunity_xref.id opportunity_id, opportunity_xref.partner_id,
+	   null enrollment_id, null program_id
 from client
 join opportunity_xref on (opportunity_xref.client_id = client.id)
 union
-select client.id client_id, null opportunity_id, null partner_id,
-	   enrollment.id enrollment_id, enrollment.program_id, enrollment.agency_id
+select enrollment.agency_id, client.id client_id, null opportunity_id, null partner_id,
+	   enrollment.id enrollment_id, enrollment.program_id
 from client
 join enrollment on (enrollment.client_id = client.id);
